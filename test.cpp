e5702d48e0890c22f48e8d8df4438865ba6c40fe
Aphcity <chalk.talisman@gmail.com>
2024-06-17 14:25:38 +0800

    Add Ssec Support, fix December issue

--- test.cpp
+++ test.cpp
@@ -14,7 +14,7 @@ int main()
     printf("%02d%02d-%02d-%02d %02d:%02d:%02d\n",
            tt_in.h_year, tt_in.l_year, tt_in.mon, tt_in.day, tt_in.hour, tt_in.min, tt_in.sec);
 
-    time_struct t_out = {20, 24, 6, 14, 12, 0, 0}; // Jun 14 2024 120000
+    time_struct t_out = {20, 24, 12, 14, 12, 0, 0}; // Jun 14 2024 120000
     uint32_t stamp = timetostamp(t_out);
     printf("Unix timestamp: %u\n", stamp);
 
--- timestamp_ssec.h
+++ timestamp_ssec.h
@@ -1,9 +1,10 @@
-typedef signed char int8_t;      /**<  8bit integer type */
-typedef signed short int16_t;    /**< 16bit integer type */
-typedef signed int int32_t;      /**< 32bit integer type */
-typedef unsigned char uint8_t;   /**<  8bit unsigned integer type */
-typedef unsigned short uint16_t; /**< 16bit unsigned integer type */
-typedef unsigned int uint32_t;   /**< 32bit unsigned integer type */
+typedef signed char int8_t;          /**<  8bit integer type */
+typedef signed short int16_t;        /**< 16bit integer type */
+typedef signed int int32_t;          /**< 32bit integer type */
+typedef unsigned char uint8_t;       /**<  8bit unsigned integer type */
+typedef unsigned short uint16_t;     /**< 16bit unsigned integer type */
+typedef unsigned int uint32_t;       /**< 32bit unsigned integer type */
+typedef unsigned long long uint64_t; /**< 64bit unsigned integer type */
 
 typedef struct time_stamp
 {
@@ -14,21 +15,22 @@ typedef struct time_stamp
     uint8_t hour;
     uint8_t min;
     uint8_t sec;
+    uint8_t ssec;
 } time_struct;
 
-uint32_t leapyear_check(uint32_t y)
+uint64_t leapyear_check(uint32_t y)
 {
     if ((0 == y % 4 && y % 100 != 0) || y % 400 == 0)
         return 1;
     return 0;
 }
 
-time_struct stamptotime(uint32_t stamp)
+time_struct stamptotime(uint64_t stamp)
 {
     const uint8_t month_buf[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     time_struct t_out;
-    uint32_t min_add, hour_add, day_add, mon_add, year_add, day_temp, year_temp;
+    uint64_t sec_add, min_add, hour_add, day_add, mon_add, year_add, day_temp, year_temp;
 
     t_out.h_year = 19;
     t_out.l_year = 70;
@@ -37,9 +39,13 @@ time_struct stamptotime(uint32_t stamp)
     t_out.hour = 0;
     t_out.min = 0;
     t_out.sec = 0;
+    t_out.ssec = 0;
 
-    min_add = (t_out.sec + stamp) / 60;
-    t_out.sec = (t_out.sec + stamp) % 60;
+    sec_add = (t_out.ssec + stamp) / 128;
+    t_out.ssec = (t_out.ssec + stamp) % 128;
+
+    min_add = (t_out.sec + sec_add) / 60;
+    t_out.sec = (t_out.sec + sec_add) % 60;
 
     hour_add = (t_out.min + min_add) / 60;
     t_out.min = (t_out.min + min_add) % 60;
@@ -52,11 +58,11 @@ time_struct stamptotime(uint32_t stamp)
 
     for (int i = t_out.mon - 1;; i++)
     {
-        size_t a = i % 12;
+        uint64_t a = i % 12;
 
         year_temp = t_out.h_year * 100 + t_out.l_year + (i / 12);
 
-        size_t temp = (a == 1 && leapyear_check(year_temp)) ? (month_buf[a] + 1) : month_buf[a];
+        uint64_t temp = (a == 1 && leapyear_check(year_temp)) ? (month_buf[a] + 1) : month_buf[a];
 
         /* If we update the month, initial the day to the '1' */
         if (mon_add)
@@ -78,8 +84,8 @@ time_struct stamptotime(uint32_t stamp)
     year_add = (mon_temp % 12 == 0) ? mon_temp / 12 - 1 : mon_temp / 12;
     t_out.mon = (mon_temp % 12 == 0) ? 12 : mon_temp % 12;
 
-    // 大于30年时重置基准至2000年1月1日0时0分0秒
-    if (year_add > 30)
+    // 大于30年时重置基准至2000年1月1日0时0分0秒0tick
+    if (year_add >= 30)
     {
         year_add -= 30;
         t_out.h_year = 20 + year_add / 100;
@@ -94,32 +100,33 @@ time_struct stamptotime(uint32_t stamp)
     return t_out;
 }
 
-uint32_t timetostamp(time_struct t_in)
+uint64_t timetostamp(time_struct t_in)
 {
     const uint8_t month_buf[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    uint32_t stamp = 0;
+    uint64_t stamp = 0, ssecperday = 86400 * 128;
     uint32_t year = t_in.h_year * 100 + t_in.l_year;
 
-    // 计算已过年的秒数
+    // 计算已过年的s秒数
     for (uint32_t y = 1970; y < year; y++)
-        stamp += leapyear_check(y) ? 366 * 86400 : 365 * 86400;
+        stamp += (uint64_t)leapyear_check(y) ? 366 * ssecperday : 365 * ssecperday;
 
-    // 计算已过月的秒数
+    // 计算已过月的s秒数
     for (uint8_t m = 0; m < t_in.mon - 1; m++)
     {
         if (m == 1 && leapyear_check(year)) // February in a leap year
-            stamp += 29 * 86400;
+            stamp += 29 * 86400 * 128;
         else
-            stamp += month_buf[m] * 86400;
+            stamp += month_buf[m] * 86400 * 128;
     }
 
-    // 计算已过天的秒数
-    stamp += (t_in.day - 1) * 86400;
+    // 计算已过天的s秒数
+    stamp += (t_in.day - 1) * 86400 * 128;
 
-    // 计算已过时分秒的秒数
-    stamp += t_in.hour * 3600;
-    stamp += t_in.min * 60;
-    stamp += t_in.sec;
+    // 计算已过时分秒的s秒数
+    stamp += t_in.hour * 3600 * 128;
+    stamp += t_in.min * 60 * 128;
+    stamp += t_in.sec * 128;
+    stamp += t_in.ssec;
 
     // 返回 int
     return stamp;

370f9414a394855c7e2a0901c56f3942766692a0
Aphcity <chalk.talisman@gmail.com>
2024-06-19 11:59:33 +0800

    keil complete version

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 #include <REG52.H> /* special function register declarations   */
-                   /* for the intended 8051 derivative         */
+/* for the intended 8051 derivative         */
 #include <stdio.h>
 #include "timestamp.h"
 
@@ -25,34 +25,30 @@ uint32_t leapyear_check(uint32_t y)
     return (0 == y % 4 && y % 100 != 0) || y % 400 == 0;
 }
 
-const uint8_t month_buf[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const uint32_t month_buf[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-uint8_t TS2RTC(uint32_t unixtimestamp, TS_time_ *RTC_out)
+uint32_t TS2RTC(uint32_t unixtimestamp, TS_time_ *RTC_out)
 {
     uint32_t min_add, hour_add, day_add, mon_add = 0, year_add = 0;
     uint32_t day_temp, mon_temp, year_temp;
-    uint32_t i;
-    uint32_t a;
-    uint32_t temp;
+    uint32_t i, a, temp;
 
     RTC_out->h_year = 19;
     RTC_out->l_year = 70;
     RTC_out->mon = 1;
     RTC_out->day = 1;
-    // RTC_out->hour = 0;
-    // RTC_out->min = 0;
-    // RTC_out->sec = 0;
-
-    // &RTC_out = {19, 70, 1, 1, 0, 0, 0};
+    RTC_out->hour = 0;
+    RTC_out->min = 0;
+    RTC_out->sec = 0;
 
     min_add = unixtimestamp / 60;
-    RTC_out->sec = unixtimestamp % 60;
+    RTC_out->sec = (uint8_t)(unixtimestamp % 60);
 
     hour_add = min_add / 60;
-    RTC_out->min = min_add % 60;
+    RTC_out->min = (uint8_t)(min_add % 60);
 
     day_add = hour_add / 24;
-    RTC_out->hour = hour_add % 24;
+    RTC_out->hour = (uint8_t)(hour_add % 24);
 
     day_temp = RTC_out->hour;
 
@@ -81,21 +77,19 @@ uint8_t TS2RTC(uint32_t unixtimestamp, TS_time_ *RTC_out)
 
     mon_temp = RTC_out->mon + mon_add;
     year_add = (mon_temp % 12 == 0) ? mon_temp / 12 - 1 : mon_temp / 12;
-    RTC_out->mon = (mon_temp % 12 == 0) ? 12 : mon_temp % 12;
+    RTC_out->mon = (uint8_t)((mon_temp % 12 == 0) ? 12 : mon_temp % 12);
 
     if (year_add > 30)
     {
         year_add -= 30;
-        RTC_out->h_year = 20 + year_add / 100;
+        RTC_out->h_year = (uint8_t)(20 + year_add / 100);
         year_add = year_add % 100;
         RTC_out->l_year = 0;
     }
     else
-    {
         RTC_out->h_year = 19;
-    }
 
-    RTC_out->l_year += year_add;
+    RTC_out->l_year += (uint8_t)year_add;
 
     if (RTC_out->h_year != 19 && RTC_out->l_year != 70 && RTC_out->mon != 1 && RTC_out->day != 1 && RTC_out->hour != 0 && RTC_out->min != 0 && RTC_out->sec != 0)
         return 1;
@@ -105,20 +99,29 @@ uint8_t TS2RTC(uint32_t unixtimestamp, TS_time_ *RTC_out)
 uint32_t timetostamp(TS_time_ t_in)
 {
     uint32_t stamp = 0;
-    uint32_t year = t_in.h_year * 100 + t_in.l_year;
+    uint32_t h_year = (uint32_t)t_in.h_year;
+    uint32_t h_yearMULTI100 = h_year * 100;
+    uint32_t l_year = (uint32_t)t_in.l_year;
+    uint32_t year = h_yearMULTI100 + l_year;
     uint32_t y;
-    uint8_t m;
+    uint32_t daySUB1 = (uint32_t)(t_in.day - 1);
+    uint32_t day2stamp = daySUB1 * 86400;
+    uint32_t hour = (uint32_t)t_in.hour;
+    uint32_t hour2stamp = hour * 3600;
+    uint32_t min2stamp = (uint32_t)t_in.min * 60;
+    uint32_t sec2stamp = (uint32_t)t_in.sec;
+    uint32_t m;
 
     for (y = 1970; y < year; y++)
         stamp += leapyear_check(y) ? 366 * 86400 : 365 * 86400;
 
-    for (m = 0; m < t_in.mon - 1; m++)
+    for (m = 0; m < (uint32_t)(t_in.mon - 1); m++)
         if (m == 1 && leapyear_check(year))
             stamp += 29 * 86400;
         else
             stamp += month_buf[m] * 86400;
 
-    stamp += (t_in.day - 1) * 86400 + t_in.hour * 3600 + t_in.min * 60 + t_in.sec;
+    stamp += day2stamp + hour2stamp + min2stamp + sec2stamp;
 
     return stamp;
 }
@@ -130,7 +133,15 @@ void main(void)
     TS_time_ t_out;
     uint32_t stamp;
 
-    unixtimestamp = 114514;
+    uint32_t t_in_h_year;
+    uint32_t t_in_l_year;
+    uint32_t t_in_mon;
+    uint32_t t_in_day;
+    uint32_t t_in_hour;
+    uint32_t t_in_min;
+    uint32_t t_in_sec;
+
+    unixtimestamp = 1704071461;
     t_out.h_year = 20;
     t_out.l_year = 24;
     t_out.mon = 1;
@@ -143,14 +154,16 @@ void main(void)
 
     while (1)
     {
-
-        // if (TS2RTC(unixtimestamp, &t_in) != -1)
-        //     printf("%02d%02d-%02d-%02d %02d:%02d:%02d\n", (int)t_in.h_year, (int)t_in.l_year, (int)t_in.mon, (int)t_in.day, (int)t_in.hour, (int)t_in.min, (int)t_in.sec);
-        // else
-        //     printf("Illegal Input!\n");
-
         TS2RTC(unixtimestamp, &t_in);
-        printf("%02d%02d-%02d-%02d %02d:%02d:%02d\n", (int)t_in.h_year, (int)t_in.l_year, (int)t_in.mon, (int)t_in.day, (int)t_in.hour, (int)t_in.min, (int)t_in.sec);
+        t_in_h_year = (uint32_t)t_in.h_year;
+        t_in_l_year = (uint32_t)t_in.l_year;
+        t_in_mon = (uint32_t)t_in.mon;
+        t_in_day = (uint32_t)t_in.day;
+        t_in_hour = (uint32_t)t_in.hour;
+        t_in_min = (uint32_t)t_in.min;
+        t_in_sec = (uint32_t)t_in.sec;
+
+        printf("%02lu%02lu-%02lu-%02lu %02lu:%02lu:%02lu\n", t_in.h_year, t_in.l_year, t_in.mon, t_in.day, t_in.hour, t_in.min, t_in.sec);
 
         stamp = timetostamp(t_out);
         printf("Unix timestamp: %lu\n", stamp);
--- timestamp.h
+++ timestamp.h
@@ -2,24 +2,25 @@
 
 typedef signed char int8_t;      /**<  8bit integer type */
 typedef signed short int16_t;    /**< 16bit integer type */
-typedef signed int int32_t;      /**< 32bit integer type */
+typedef signed long int32_t;      /**< 32bit integer type */
 typedef unsigned char uint8_t;   /**<  8bit unsigned integer type */
 typedef unsigned short uint16_t; /**< 16bit unsigned integer type */
-typedef unsigned int uint32_t;   /**< 32bit unsigned integer type */
+typedef unsigned long uint32_t;   /**< 32bit unsigned integer type */
+//typedef unsigned long uint64_t;  /**< 64bit unsigned integer type */
 
 typedef struct
 {
-    uint8_t h_year;
-    uint8_t l_year;
-    uint8_t mon;
-    uint8_t day;
-    uint8_t hour;
-    uint8_t min;
-    uint8_t sec;
+    uint32_t h_year;
+    uint32_t l_year;
+    uint32_t mon;
+    uint32_t day;
+    uint32_t hour;
+    uint32_t min;
+    uint32_t sec;
 } TS_time_;
 
 extern uint32_t leapyear_check(uint32_t y);
 
 extern uint32_t timetostamp(TS_time_ t_in);
 
-extern uint8_t TS2RTC(uint32_t unixtimestamp, TS_time_ *RTC_out);
+extern uint32_t TS2RTC(uint32_t unixtimestamp, TS_time_ *RTC_out);
